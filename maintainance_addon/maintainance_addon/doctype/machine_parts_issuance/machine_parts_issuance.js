1ffc644c315ed4ae881afcdc5cce820d271cc2c3
Mohtashim-1m <shoaibmohtashim973@gmail.com>
2024-06-11 13:07:49 +0000

    feat: script created based on another child table values

--- maintainance_addon/maintainance_addon/doctype/machine_parts_issuance/machine_parts_issuance.js
+++ maintainance_addon/maintainance_addon/doctype/machine_parts_issuance/machine_parts_issuance.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Machine Parts Issuance', {
-	// refresh: function(frm) {
-
-	// }
+	refresh(frm) {
+		frm.fields_dict['machine_part_returned'].grid.get_field('part_name').get_query = function (doc, cdt, cdn) {
+			var part_name_related = get_part_name_related(frm);
+			return {
+				filters: [
+					["Item", "item_code", "in", part_name_related]
+				]
+			};
+		}
+	}
 });
+
+// Function to get related part names from machine_part_details
+function get_part_name_related(frm) {
+	var part_name_related = [];
+	frm.doc.machine_part_details.forEach(row => {
+		if (row.item_code) {
+			part_name_related.push(row.item_code);
+		}
+	});
+	return part_name_related;
+}
+
