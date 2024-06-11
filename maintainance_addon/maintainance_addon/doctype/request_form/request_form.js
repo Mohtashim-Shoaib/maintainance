6ddae3d077cf8ca8617e3aa06cdcc2d50781d638
Mohtashim-1m <shoaibmohtashim973@gmail.com>
2024-06-11 08:09:34 +0000

    fix: error

--- maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
+++ maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
@@ -1,8 +1,122 @@
 // Copyright (c) 2024, mohtashim and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Request Form', {
-	// refresh: function(frm) {
+frappe.ui.form.on('Parts Request', {
+	refresh(frm) {
+		cur_frm.fields_dict["items"].grid.get_field("item_code").get_query = function (doc) {
+			return {
+				filters: {
+					"item_category": "General"
+				}
+			}
+		}
+	}
+})
 
-	// }
+
+frappe.ui.form.on('Parts Request CT', {
+	item_code: function (frm, cdt, cdn) {
+		var d = locals[cdt][cdn];
+		frappe.call({
+			method: 'erpnext.maintenance.doctype.machine_parts.machine_parts.get_balance_qty',
+			args: {
+				item: d.item_code
+			},
+			callback: function (r) {
+				console.log(r)
+				d.balance_qty = r.message
+
+				frm.refresh_field("machine_part_details")
+			}
+		})
+	},
+
+
+
+
 });
+
+frappe.ui.form.on('Parts Request CT', {
+	item_code(frm, cdt, cdn) {
+		set_rate(frm, cdt, cdn);
+
+	},
+	qty(frm, cdt, cdn) {
+		set_order(frm, cdt, cdn);
+
+	}
+
+});
+function set_order(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	if (d.qty > d.balance_qty) {
+		frappe.model.set_value(d.doctype, d.name, "required_qty", d.qty - d.balance_qty)
+	}
+	else {
+		frappe.model.set_value(d.doctype, d.name, "required_qty", 0)
+	}
+}
+function set_rate(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	frappe.call({
+		method: "frappe.client.get_value",
+		args: {
+			doctype: "Item Price",
+			filters: {
+				item_code: d.part_name,
+				price_list: "Standard Buying"
+			},
+			fieldname: ["price_list_rate"]
+		},
+		callback: function (r) {
+			frappe.model.set_value(d.doctype, d.name, "rate", r.message["price_list_rate"])
+
+		}
+	});
+}
+
+
+frappe.ui.form.on('Parts Request', {
+	refresh: function (frm) {
+		var me = frm
+		frm.add_custom_button(__('Create MR'),
+			function () {
+				frappe.call({
+					method: 'maintainance_addon.maintainance_addon.doctype.parts_request.parts_request.make_mr',
+					args: {
+						parts_request: frm.doc.name
+					},
+					callback: function (r) {
+						if (r.message) {
+							frappe.msgprint(__('Items updated successfully.'));
+							frm.reload_doc();
+							console.log("test")
+						}
+
+					}
+				});
+
+
+
+			}, __("Create"));
+		frm.add_custom_button(__('Parts Issuance'),
+			function () {
+				frappe.call({
+					method: 'erpnext.maintenance.doctype.parts_request.parts_request.make_issuance',
+					args: {
+						parts_request: frm.doc.name
+					},
+					callback: function (r) {
+						if (r.message) {
+							frappe.msgprint(__('Items updated successfully.'));
+							frm.reload_doc();
+						}
+					}
+				});
+
+
+
+			}, __("Create"));
+	}
+});
+
--- maintainance_addon/maintainance_addon/doctype/request_form/test_request_form.py
+++ maintainance_addon/maintainance_addon/doctype/request_form/test_request_form.py
@@ -1,9 +1,81 @@
 # Copyright (c) 2024, mohtashim and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+# Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
 
+import frappe
+from frappe.model.document import Document
 
-class TestRequestForm(FrappeTestCase):
-	pass
+class PartsRequest(Document):
+    pass
+    def before_submit(self):
+        self.make_part_issuance()
+        self.make_gi_issuance()
+    def make_part_issuance(self):
+        relevant_items_exist = False
+        for row in self.items:
+            item_group = frappe.db.get_value('Item', row.item_code, 'item_group')
+            if item_group == "MACHINE SPARE PARTS":
+                relevant_items_exist = True
+                break
+        if relevant_items_exist:
+            mr = frappe.new_doc("Machine Parts")
+            mr.date = self.posting_date
+            mr.by_hand = "ABDUL REHMAN"
+            for row in self.items:
+                item_group = frappe.db.get_value('Item', row.item_code, ['item_group'])
+                if item_group == "MACHINE SPARE PARTS":
+                    mri= mr.append('requested_items')
+                    mri.item_code = row.item_code
+                    mri.requested_qty = row.required_qty
+            mr.save()
+    def make_gi_issuance(self):
+        relevant_items_exist = False
+        for row in self.items:
+            item_group = frappe.db.get_value('Item', row.item_code, 'item_group')
+            if item_group == "ELECTRIC ITEMS" or "GENERAL ITEM" or "STATIONERY ITEMS" or "TOOLS":
+                relevant_items_exist = True
+                break
+        if relevant_items_exist:
+            mr = frappe.new_doc("General Item Issuance")
+            mr.date = self.posting_date
+            mr.by_hand = "ABDUL REHMAN"
+            mr.user = self.request_by
+            for row in self.items:
+                item_group = frappe.db.get_value('Item', row.item_code, 'item_group')
+                if item_group in ["ELECTRIC ITEMS", "GENERAL ITEM", "STATIONERY ITEMS", "TOOLS"]:
+                    mri= mr.append('general_item_issuance_ct')
+                    mri.part_name = row.item_code
+                    mri.qty = row.required_qty
+                    mri.department = "Production - SAH"
+            mr.save()
+@frappe.whitelist()
+def make_mr(parts_request):
+    doc = frappe.get_doc("Parts Request", parts_request)
+    mr = frappe.new_doc("Material Request")
+    mr.material_request_type = "Purchase"
+    mr.transaction_date = doc.posting_date
+    mr.set_warehouse = "Stores - SAH"
+    mri= mr.append('items')
+    for row in doc.items:
+        if row.required_qty > 0:
+            mri.item_code = row.item_code
+            mri.qty = row.required_qty
+            mri.schedule_date = doc.posting_date
+            mri.uom = frappe.db.get_value('item', row.item_code, 'stock_uom')
+    mr.save()
+    frappe.msgprint("Material Request created!")
+@frappe.whitelist()
+def make_issuance(parts_request):
+    doc = frappe.get_doc("Parts Request", parts_request)
+    mr = frappe.new_doc("Machine Parts")
+    mr.date = doc.posting_date
+    mr.by_hand = "ABDUL REHMAN"
+    mri= mr.append('machine_part_details')
+    for row in doc.items:
+        mri.item_code = row.item_code
+        mri.required_qty = row.balance_qty
+        mri.department = "Production - SAH"
+    mr.save()
+    frappe.msgprint("Parts Issurance created!")
