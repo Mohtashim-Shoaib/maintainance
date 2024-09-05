889b0746db80e4dc350ab41f1d0f058563ce6db6
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-09-05 08:54:46 +0000

    chore: add qty option in prompt in request form

--- maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
+++ maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
@@ -1,33 +1,5 @@
-// Copyright (c) 2024, mohtashim and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Request Form', {
-	refresh(frm) {
-		cur_frm.fields_dict["items"].grid.get_field("item_code").get_query = function (doc) {
-			return {
-				filters: {
-					"item_group": "General"
-				}
-			}
-		}
-	}
-})
-
-
-var get_prompt_fields = () => {
-	return [
-		{
-			label: __("Item Code"),
-			fieldname: "item_code",
-			fieldtype: "Link",
-			options: "Item"
-		}
-	];
-};
-
 frappe.ui.form.on('Request Form', {
     refresh(frm) {
-        // Setup filter
         cur_frm.fields_dict["items"].grid.get_field("item_code").get_query = function (doc) {
             return {
                 filters: {
@@ -39,9 +11,8 @@ frappe.ui.form.on('Request Form', {
         frm.add_custom_button(__('Get Available Stock'), () => {
             let fields = get_prompt_fields();
             frappe.prompt(fields, (values) => {
-                // Handle the values from the prompt
-                get_available_stock(frm, values.item_code);
-                get_available_stock_1(frm, values.item_code);
+                get_available_stock(frm, values.item_code, values.qty);
+                get_available_stock_1(frm, values.item_code, values.qty);
             }, __("Set Values"), __("Get Stock"));
         });
     }
@@ -54,17 +25,23 @@ var get_prompt_fields = () => {
             fieldname: "item_code",
             fieldtype: "Link",
             options: "Item"
+        },
+        {
+            label: __("Quantity"),
+            fieldname: "qty",
+            fieldtype: "Int"
         }
     ];
 };
 
-function get_available_stock(frm, item_code) {
+function get_available_stock(frm, item_code, qty) {
     frappe.db.get_doc("Item", item_code).then(doc => {
         var group = doc.item_group;
-        const addItemToTable = (itemCode, balanceQty) => {
+        const addItemToTable = (itemCode, balanceQty, qty) => {
             let child = frm.add_child("items", {
                 "item_code": itemCode,
-                "balance_qty": balanceQty ? balanceQty : 0
+                "balance_qty": balanceQty ? balanceQty : 0,
+                "qty": qty 
             });
             frm.refresh_field("items");
         };
@@ -74,20 +51,21 @@ function get_available_stock(frm, item_code) {
                 method: "maintainance_addon.maintainance_addon.doctype.request_form.request_form.get_available_qty",
                 args: { item_code: item_code },
                 callback: function (r) {
-                    addItemToTable(item_code, r.message);
+                    addItemToTable(item_code, r.message, qty);
                 }
             });
         }
     });
 }
 
-function get_available_stock_1(frm, item_code) {
+function get_available_stock_1(frm, item_code, qty) {
     frappe.db.get_doc("Item", item_code).then(doc => {
         var group = doc.item_group;
-        const addItemToTable = (itemCode, balanceQty) => {
+        const addItemToTable = (itemCode, balanceQty, qty) => {
             let child = frm.add_child("item", {
                 "item_code": itemCode,
-                "balance_qty": balanceQty ? balanceQty : 0
+                "balance_qty": balanceQty ? balanceQty : 0,
+                "qty": qty 
             });
             frm.refresh_field("item");
         };
@@ -97,9 +75,10 @@ function get_available_stock_1(frm, item_code) {
                 method: "maintainance_addon.maintainance_addon.doctype.request_form.request_form.get_available_qty",
                 args: { item_code: item_code },
                 callback: function (r) {
-                    addItemToTable(item_code, r.message);
+                    addItemToTable(item_code, r.message, qty);
                 }
             });
         }
     });
 }
+
