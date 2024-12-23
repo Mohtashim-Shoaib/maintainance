4605d7d744ce5c0781caa434ea230a5f88b7543d
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-12-23 17:33:09 +0500

    chore: fix in ct request form

--- maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
+++ maintainance_addon/maintainance_addon/doctype/request_form/request_form.js
@@ -88,9 +88,9 @@ frappe.ui.form.on('Request Form', {
 	refresh:function (frm){
 		frm.set_df_property('items','cannot_add_rows',1)
 		frm.set_df_property('item','cannot_add_rows',1)
-		frm.set_df_property('items','read_only',1)
-        frm.set_df_property('items','cannot_delete_rows',1)
-        frm.set_df_property('item','read_only',1)
-        frm.set_df_property('item','cannot_delete_rows',1)
+		// frm.set_df_property('items','read_only',1)
+        // frm.set_df_property('items','cannot_delete_rows',1)
+        // frm.set_df_property('item','read_only',1)
+        // frm.set_df_property('item','cannot_delete_rows',1)
 	}
 	});
